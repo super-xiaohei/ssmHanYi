5457fcf0ca678891a3cad6a38f4df3dfeab8d25f
郑义桥 <1312890342@qq.com>
2020-11-19 09:13:37 +0800

    2020-11-18 在mapper的扩展包里面加入了一个批量删除的方法

--- src/main/java/com/suncaper/demo/mapper/extend/BatchMapperExtend.java
+++ src/main/java/com/suncaper/demo/mapper/extend/BatchMapperExtend.java
@@ -1,8 +1,14 @@
 package com.suncaper.demo.mapper.extend;
 
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * @author zyq
  * @date 2020/11/16 - 18:40
  */
 public interface BatchMapperExtend {
+
+    void deletes(@Param("ids") List<Long> ids);
 }
