f68d4933f532043cc549101cbdf883d6646dd7e6
郑义桥 <1312890342@qq.com>
2020-11-19 09:28:52 +0800

    2020-11-18 添加了end,deletes,listByAjax三个方法

--- src/main/java/com/suncaper/demo/service/BatchService.java
+++ src/main/java/com/suncaper/demo/service/BatchService.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageInfo;
 import com.suncaper.demo.entity.Batch;
 import com.suncaper.demo.entity.dto.BatchDto;
 
+import java.util.List;
+
 /**
  * @author zyq
  * @date 2020/11/16 - 18:38
@@ -14,4 +16,12 @@ public interface BatchService {
     Integer save(Batch batch);
 
     PageInfo<Batch> list(BatchDto batchDto);
+
+    void end(long id);
+
+    void deletes(List<Long> ids);
+
+    List<Batch> listByAjax();
+
+
 }
