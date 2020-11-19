d80a1029cb23d79f95ebe592e82bfe37e9402cd0
郑义桥 <1312890342@qq.com>
2020-11-19 09:15:50 +0800

    2020-11-18 新添加的三个mapperExtend接口(自己写的)

---
+++ src/main/java/com/suncaper/demo/mapper/extend/ImageMapperExtend.java
@@ -0,0 +1,8 @@
+package com.suncaper.demo.mapper.extend;
+
+/**
+ * @author zyq
+ * @date 2020/11/18 - 17:58
+ */
+public interface ImageMapperExtend {
+}
---
+++ src/main/java/com/suncaper/demo/mapper/extend/SkuMapperExtend.java
@@ -0,0 +1,9 @@
+package com.suncaper.demo.mapper.extend;
+
+/**
+ * @author zyq
+ * @date 2020/11/18 - 17:58
+ */
+public interface SkuMapperExtend {
+
+}
