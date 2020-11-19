21b3973f2848dba507c422b5fe65402de431c94f
郑义桥 <1312890342@qq.com>
2020-11-19 09:29:31 +0800

    2020-11-18 添加了getDictionary()方法

--- src/main/java/com/suncaper/demo/service/DictionaryService.java
+++ src/main/java/com/suncaper/demo/service/DictionaryService.java
@@ -1,5 +1,10 @@
 package com.suncaper.demo.service;
 
+import com.suncaper.demo.common.JsonResult;
+import com.suncaper.demo.entity.Dictionary;
+
+import java.util.List;
+
 /**
  * @author zyq
  * @date 2020/11/17 - 15:59
@@ -7,4 +12,7 @@ package com.suncaper.demo.service;
 public interface DictionaryService {
 
     String getNameByValueAndType(String type,String value);
+
+    List<Dictionary> getDictionary(Dictionary dictionary);
+
 }
