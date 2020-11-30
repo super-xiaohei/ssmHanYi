bd51fce571af74c0c01986be72bdc21d03d85c9b
郑义桥 <1312890342@qq.com>
2020-11-30 17:26:59 +0800

    2020-11-30 添加dto包

--- src/main/java/com/suncaper/demo/entity/dto/ProductDto.java
+++ src/main/java/com/suncaper/demo/entity/dto/ProductDto.java
@@ -1,6 +1,6 @@
 package com.suncaper.demo.entity.dto;
 
-import com.suncaper.demo.entity.Batch;
+import com.suncaper.demo.entity.Product;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -8,14 +8,14 @@ import java.util.List;
 
 /**
  * @author zyq
- * @date 2020/11/17 - 17:44
+ * @date 2020/11/19 - 11:07
  */
 @Data
 @Accessors(chain = true)
-public class BatchDto extends Batch {
-
-    private List<Long> ids;
+public class ProductDto extends Product {
+    private List<String> skuName;
     private Integer pageNum;
     private Integer pageSize;
+    private List<String> oldImg;
 
 }
