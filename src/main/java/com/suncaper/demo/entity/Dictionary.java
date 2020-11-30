15521d28b1d3e278ee704fad5bf2bd7e7befd65d
郑义桥 <1312890342@qq.com>
2020-11-30 17:30:43 +0800

    2020-11-30 实体类，使用了lombok，可以不适用getter和setter了，简化了代码

--- src/main/java/com/suncaper/demo/entity/Dictionary.java
+++ src/main/java/com/suncaper/demo/entity/Dictionary.java
@@ -1,7 +1,12 @@
 package com.suncaper.demo.entity;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 import java.util.Date;
 
+@Data
+@Accessors(chain = true)
 public class Dictionary {
     private Integer id;
 
@@ -29,107 +34,5 @@ public class Dictionary {
 
     private Date gmtModified;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Integer parentId) {
-        this.parentId = parentId;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type == null ? null : type.trim();
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName == null ? null : itemName.trim();
-    }
-
-    public String getItemValue() {
-        return itemValue;
-    }
-
-    public void setItemValue(String itemValue) {
-        this.itemValue = itemValue == null ? null : itemValue.trim();
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
-
-    public String getExtdata() {
-        return extdata;
-    }
-
-    public void setExtdata(String extdata) {
-        this.extdata = extdata == null ? null : extdata.trim();
-    }
-
-    public Short getSortId() {
-        return sortId;
-    }
-
-    public void setSortId(Short sortId) {
-        this.sortId = sortId;
-    }
-
-    public Boolean getIsEditable() {
-        return isEditable;
-    }
-
-    public void setIsEditable(Boolean isEditable) {
-        this.isEditable = isEditable;
-    }
-
-    public Boolean getIsDeletable() {
-        return isDeletable;
-    }
-
-    public void setIsDeletable(Boolean isDeletable) {
-        this.isDeletable = isDeletable;
-    }
-
-    public Boolean getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(Boolean isDeleted) {
-        this.isDeleted = isDeleted;
-    }
-
-    public Date getGmtCreated() {
-        return gmtCreated;
-    }
-
-    public void setGmtCreated(Date gmtCreated) {
-        this.gmtCreated = gmtCreated;
-    }
-
-    public Date getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Date gmtModified) {
-        this.gmtModified = gmtModified;
-    }
 }
