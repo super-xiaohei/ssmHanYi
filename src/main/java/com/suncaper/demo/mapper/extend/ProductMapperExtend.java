373362f2575c752a5794ac0455c874e24c2685ab
郑义桥 <1312890342@qq.com>
2020-11-30 17:32:51 +0800

    2020-11-30 修改

--- src/main/java/com/suncaper/demo/mapper/extend/ProductMapperExtend.java
+++ src/main/java/com/suncaper/demo/mapper/extend/ProductMapperExtend.java
@@ -1,8 +1,12 @@
 package com.suncaper.demo.mapper.extend;
 
+import com.suncaper.demo.entity.dto.ProductDto;
+
 /**
  * @author zyq
  * @date 2020/11/18 - 17:58
  */
 public interface ProductMapperExtend {
+
+    void MyInsertSelective(ProductDto productDto);
 }
--- src/main/java/com/suncaper/demo/mapper/extend/SkuMapperExtend.java
+++ src/main/java/com/suncaper/demo/mapper/extend/SkuMapperExtend.java
@@ -1,9 +1,15 @@
 package com.suncaper.demo.mapper.extend;
 
+import com.suncaper.demo.entity.Sku;
+import com.suncaper.demo.mapper.SkuMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * @author zyq
  * @date 2020/11/18 - 17:58
  */
 public interface SkuMapperExtend {
-
+    void ManyInsert(@Param("skuList") List<Sku> skuList);
 }
