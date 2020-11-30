afce6fd0759efe9e25eba50b2957eb8925a1c149
郑义桥 <1312890342@qq.com>
2020-11-30 17:32:16 +0800

    2020-11-30 扩展

---
+++ src/main/java/com/suncaper/demo/mapper/extend/ApplicationMapperExtend.java
@@ -0,0 +1,12 @@
+package com.suncaper.demo.mapper.extend;
+
+import com.suncaper.demo.entity.vo.StatisticsVo;
+
+/**
+ * @author zyq
+ * @date 2020/11/30 - 11:43
+ */
+public interface ApplicationMapperExtend {
+
+    StatisticsVo statistics();
+}
