1041225d1af8e01526fe8f5e3705d0384821dace
郑义桥 <1312890342@qq.com>
2020-12-07 11:02:16 +0800

    2020-12-7 补充功能:                     批次增加参与年级,仅有所发布的年级的学生才能申请;                     寒衣款式增加总量与库存量,当库存量为0时,学生可见衣服但不可选.

--- src/main/java/com/suncaper/demo/entity/extend/BatchExtend.java
+++ src/main/java/com/suncaper/demo/entity/extend/BatchExtend.java
@@ -3,6 +3,8 @@ package com.suncaper.demo.entity.extend;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.util.List;
+
 /**
  * @author zyq
  * @date 2020/11/17 - 16:44
@@ -11,5 +13,6 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class BatchExtend {
     private String diffcultyValue;
+    private List<String> grades;
 
 }
